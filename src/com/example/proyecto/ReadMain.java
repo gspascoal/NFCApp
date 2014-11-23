5c54c50f6a42b47e611f9c6315414763169088d7
Rafael Machado <mchdr19@gmail.com>
2014-11-23 01:09:11 -0430

    Foreground Tag Detection

--- src/com/example/proyecto/ReadMain.java
+++ src/com/example/proyecto/ReadMain.java
@@ -4,13 +4,18 @@ import java.nio.charset.Charset;
 
 import android.app.Activity;
 import android.app.PendingIntent;
+import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.content.IntentFilter.MalformedMimeTypeException;
+import android.net.Uri;
 import android.nfc.NdefMessage;
 import android.nfc.NdefRecord;
 import android.nfc.NfcAdapter;
 import android.nfc.Tag;
+import android.nfc.tech.MifareUltralight;
+import android.nfc.tech.Ndef;
+import android.nfc.tech.NfcA;
 import android.nfc.tech.NfcF;
 import android.os.Bundle;
 import android.os.Parcelable;
@@ -44,7 +49,7 @@ public class ReadMain extends Activity {
 			status.setText("NFC is available for the device");
 		}
 		
-		
+		/*
 		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(getIntent().getAction())) {
 			Log.d("debug","NDEF Discovered");
 			Tag detectedTag = getIntent().getParcelableExtra(NfcAdapter.EXTRA_TAG);
@@ -78,26 +83,30 @@ public class ReadMain extends Activity {
 		else{
 			Log.d("debug", "Nothing detected");
 		}
+		*/
 		
-		/*
 		pendingIntent = PendingIntent.getActivity(
 			    this, 0, new Intent(this, getClass()).addFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP), 0);
 		
-		IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);*/
+		IntentFilter ndef = new IntentFilter(NfcAdapter.ACTION_NDEF_DISCOVERED);
 	   
-		//try {
-	        //ndef.addDataType("*/*");    /* Handles all MIME based dispatches.
-	                                      // You should specify only the ones that you need. 
-	    //}
-	                                       /*
+		try {
+	       ndef.addDataType("*/*");    /* Handles all MIME based dispatches.
+	         //                             You should specify only the ones that you need. */
+	       // ndef.addDataScheme("http");
+	       //ndef.addAction(Intent.ACTION_VIEW);
+	    }
+	                                       
 	    catch (MalformedMimeTypeException e) {
 	        throw new RuntimeException("fail", e);
 	    }
 	    intentFiltersArray = new IntentFilter[] {ndef, };
 	   
-	    techListsArray = new String[][] { new String[] { NfcF.class.getName() } };*/
-		/*
+	    techListsArray = new String[][] { new String[] { NfcA.class.getName() , 
+	    		Ndef.class.getName(), 
+	    		MifareUltralight.class.getName() } };
 		
+		/*
 		if (savedInstanceState == null) {
 			/*getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
@@ -107,7 +116,8 @@ public class ReadMain extends Activity {
 	private NdefMessage[] getNdefMessages(Intent intent) {
 		// TODO Auto-generated method stub
 		NdefMessage[] message = null;
-		if (NfcAdapter.ACTION_NDEF_DISCOVERED.equals(intent.getAction())) {
+		if (NfcAdapter.ACTION_TECH_DISCOVERED.equals(intent.getAction())) {
+			Log.d("", "I found some shit.");
 			Parcelable[] rawMessages = intent.getParcelableArrayExtra(NfcAdapter.EXTRA_NDEF_MESSAGES);
 			if (rawMessages != null) {
 				message = new NdefMessage[rawMessages.length];
@@ -127,7 +137,7 @@ public class ReadMain extends Activity {
 		return message;
 	}
 
-	/*
+	
 	public void onPause() {
 	    super.onPause();
 	   myNfcAdapter.disableForegroundDispatch(this);
@@ -136,34 +146,70 @@ public class ReadMain extends Activity {
 	public void onResume() {
 	    super.onResume();
 	   myNfcAdapter.enableForegroundDispatch(this, pendingIntent, intentFiltersArray, techListsArray);
-	}*/
+	}
 
 	public void onNewIntent(Intent intent) {
 	    Tag tagFromIntent = intent.getParcelableExtra(NfcAdapter.EXTRA_TAG);
 	    //do something with tagFromIntent
 	    Log.d("debug", "tag detected");
 		 // GET NDEF MESSAGE IN THE TAG
-		 NdefMessage[] messages = getNdefMessages(getIntent());
+		 NdefMessage[] messages = getNdefMessages(intent);
 		 
 		 // PROCESS NDEF MESSAGE
 		 String payload = null;
-		 byte payloadHeader ;
+		 byte payloadHeader = 0x00;
+		 if (messages != null) {
+			 for (int i = 0; i < messages.length; i++) {
+				 status.append("\n");
+				 	status.append("Message "+(i+1)+" \n");
+				 	for (int j = 0; j < messages[0].getRecords().length; j++) {
+				 		NdefRecord record = messages[i].getRecords()[j];
+				 		status.append((j+1)+"th. Record Tnf: "+record.getTnf() +"\n");
+				 		status.append((j+1)+"th. Record type: "+record.getType()+"\n");
+				 		status.append((j+1)+"th. Record id: "+record.getId()+"\n");
+				 		status.append((j+1)+"th. Record Contents: "+record.describeContents()+"\n");
+				 		
+				 		
+				 		payload = new String(record.getPayload(), 1, record.getPayload().length-1, Charset.forName("UTF-8"));
+				 		status.append((j+1)+"th. Record payload:  "+payload +"\n");
+				 		payloadHeader = record.getPayload()[0];
+				 		status.append((j+1)+"th. Record payload header:  "+payloadHeader +"\n");
+				 	}
+				 }
+		
+			 if (payloadHeader == 0x01) {
+					Intent dataIntent = new Intent();
+					dataIntent.setAction(Intent.ACTION_VIEW);
+					dataIntent.setData(Uri.parse("http://"+payload));
+					try {
+						Log.d("debug", "Yes, I did it");
+						startActivity(dataIntent);
+						
+					} catch (ActivityNotFoundException e) {
+						// TODO: handle exception
+						
+						return;
+					}
+				}
 		 
-		 for (int i = 0; i < messages.length; i++) {
-		 	status.append("Message "+(i+1)+" \n");
-		 	for (int j = 0; j < messages[0].getRecords().length; j++) {
-		 		NdefRecord record = messages[i].getRecords()[j];
-		 		status.append((j+1)+"th. Record Tnf: "+record.getTnf()+"\n");
-		 		status.append((j+1)+"th. Record type: "+record.getType()+"\n");
-		 		status.append((j+1)+"th. Record id: "+record.getId()+"\n");
-		 		
-		 		payload = new String(record.getPayload(), 1, record.getPayload().length-1, Charset.forName("UTF-8"));
-		 		status.append((j+1)+"th. Record payload:  "+payload +"\n");
-		 		payloadHeader = record.getPayload()[0];
-		 		status.append((j+1)+"th. Record payload header:  "+payloadHeader +"\n");
-		 	}
 		 }
-	    
+		 else {
+			Log.d("debug", "that shit is so empty");
+		}
+		 
+	    /*
+		 if (payloadHeader == 0x01) {
+			Intent dataIntent = new Intent();
+			dataIntent.setAction(Intent.ACTION_VIEW);
+			dataIntent.setData(Uri.parse("http://"+payload));
+			try {
+				startActivity(dataIntent);
+				Log.d("debug", "Yes, I did it");
+			} catch (ActivityNotFoundException e) {
+				// TODO: handle exception
+				return;
+			}
+		}*/
 	}
 	
 	
