835cf977b06c7b1bef8e2ac8daf23fe181080410
Rafael Machado <mchdr19@gmail.com>
2015-01-08 17:17:49 -0430

    Tag Content Create Form Updated

--- src/com/example/proyecto/CreateTagContent.java
+++ src/com/example/proyecto/CreateTagContent.java
@@ -59,7 +59,7 @@ public class CreateTagContent extends Activity implements OnItemSelectedListener
 	private final static int PICK_CONTACT = 1;
 	public Map<String, String> PLH =  new LinkedHashMap<String,String>();
 	public Map<String, Integer> LNI =  new LinkedHashMap<String,Integer>();
-	
+	private String currentSelection;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -172,6 +172,7 @@ public class CreateTagContent extends Activity implements OnItemSelectedListener
 	public void onItemSelected(AdapterView<?> parent, View view, int position,long id) {
 		// TODO Auto-generated method stub
 		String kind = parent.getItemAtPosition(position).toString();
+		currentSelection = kind;
 		Log.d("debug", "item selected: "+ kind);
 		int layoutId = LNI.get(kind);
 		form =  new Form(this, layoutId);
@@ -242,6 +243,18 @@ public class CreateTagContent extends Activity implements OnItemSelectedListener
 				                int position, long id) {
 				              final String item = (String) parent.getItemAtPosition(position);
 				              fieldPhone.setText(item);
+				              /*
+				              switch (currentSelection) {
+							case "Telephone Number":
+								
+								break;
+							case "SMS":
+								fieldR.setText(item);
+								break;	
+							default:
+								break;
+							}*/
+				              
 				              dialog.dismiss();
 				            }
 
@@ -282,18 +295,44 @@ public class CreateTagContent extends Activity implements OnItemSelectedListener
 	public NdefMessage createTagContent(String kind){
 
 		NdefMessage newMessage =  null;
+		byte[] uriField;
+		byte[] payload;
+		NdefRecord uriRecord; 
+		String content;
 	    switch (kind) {
 		case "Telephone Number":
 			EditText fieldPhone =  (EditText)findViewById(R.id.fieldPhone);
 			String telNumber = fieldPhone.getText().toString();
-			byte[] uriField =  telNumber.getBytes();
-			byte[] payload = new byte[uriField.length + 1];
+			uriField =  telNumber.getBytes();
+			payload = new byte[uriField.length + 1];
 			payload[0] = 0x05; //Code for tel:
 			System.arraycopy(uriField, 0, payload, 1, uriField.length);
-			NdefRecord uriRecord = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_URI, new byte[0], payload);
+			uriRecord = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_URI, new byte[0], payload);
 			newMessage = new NdefMessage(new NdefRecord[] {uriRecord});
 			break;
-
+		case "Email":
+			EditText fieldTo =  (EditText)findViewById(R.id.fieldTo);
+			EditText fieldSubject =  (EditText)findViewById(R.id.fieldSubject);
+			EditText fieldBody =  (EditText)findViewById(R.id.fieldBody);
+			content = fieldTo.getText().toString() + "?subject="+fieldSubject.getText().toString()+"&body="+fieldBody.getText().toString();
+			uriField =  content.getBytes();
+			payload = new byte[uriField.length + 1];
+			payload[0] = 0x06; // Code for mailto:
+			System.arraycopy(uriField, 0, payload, 1, uriField.length);
+			uriRecord = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_URI, new byte[0], payload);
+			newMessage = new NdefMessage(new NdefRecord[] {uriRecord});
+			break;
+		case "SMS":
+			EditText fieldReceiver =  (EditText)findViewById(R.id.fieldPhone);
+			EditText fieldMessage =  (EditText)findViewById(R.id.fieldMessage);
+			content = "sms:"+fieldReceiver.getText().toString() + "?body="+fieldMessage.getText().toString();
+			uriField =  content.getBytes();
+			payload = new byte[uriField.length + 1];
+			payload[0] = 0x00; // Code for sms:
+			System.arraycopy(uriField, 0, payload, 1, uriField.length);
+			uriRecord = new NdefRecord(NdefRecord.TNF_WELL_KNOWN, NdefRecord.RTD_URI, new byte[0], payload);
+			newMessage = new NdefMessage(new NdefRecord[] {uriRecord});
+			break;
 		default:
 			break;
 		}
